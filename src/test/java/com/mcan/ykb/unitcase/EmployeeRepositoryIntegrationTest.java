6d17ab569d091214e5eac897d2691ffd56a387d5
enemymerch <mcan.yilmaz@windowslive.com>
2019-10-27 17:54:52 +0300

    Test todo added

--- src/test/java/com/mcan/ykb/unitcase/EmployeeRepositoryIntegrationTest.java
+++ src/test/java/com/mcan/ykb/unitcase/EmployeeRepositoryIntegrationTest.java
@@ -6,7 +6,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest(EmployeeController.class)
 public class EmployeeRepositoryIntegrationTest {
 
 }
